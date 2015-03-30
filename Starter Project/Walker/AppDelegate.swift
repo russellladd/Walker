7a04d1648f922a00f8ceac2007ac2b435ac36ff4
Russell Ladd <russell.ladd@grl5.com>
2015-03-30 17:44:23 -0400

    Added starter project

---
+++ Starter Project/Walker/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Walker
+//
+//  Created by Russell Ladd on 3/28/15.
+//  Copyright (c) 2015 GRL5. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: Window
+    
+    var window: UIWindow?
+    
+    // MARK: Application life cycle
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        return true
+    }
+}
